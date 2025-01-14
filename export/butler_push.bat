b4ea64017088fa1fc07b3bd8e261eedb3819464e
Harmony Honey Monroe <harmonymonroe@proton.me>
2025-01-14 01:47:47 -0500

    remove steam & itch scripts (-:

--- export/butler_push.bat
+++
@@ -1,6 +0,0 @@
-butler push win harmonyhoney/rota:win
-butler push linux harmonyhoney/rota:linux
-butler push mac harmonyhoney/rota:mac
-butler push web harmonyhoney/rota:web
-butler push android harmonyhoney/rota:android
-pause
